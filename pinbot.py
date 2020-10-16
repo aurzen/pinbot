5df9107bf16d144662f47e5d7b06c92fb6c436c0
Zenith <z@zenith.dev>
2020-10-16 09:31:33 -0700

    pinbot 2.1.13

--- pinbot.py
+++ pinbot.py
@@ -15,26 +15,27 @@ log = logging.Logger("a")
 log.setLevel(logging.DEBUG)
 
 if ty.TYPE_CHECKING:
-    from aurflux.context import MessageCtx
+   from aurflux.context import MessageCtx
 
 
 class Pinbot:
-    def __init__(self):
-        self.event_router = aurcore.event.EventRouterHost(name=self.__class__.__name__.lower())
-        self.flux = aurflux.FluxClient("pinbot", admin_id=TOKENS.ADMIN_ID, parent_router=self.event_router, status="..help|s.ze.ax/d for support|pm for invite")
+   def __init__(self):
+      self.event_router = aurcore.event.EventRouterHost(name=self.__class__.__name__.lower())
+      self.flux = aurflux.FluxClient(
+         "pinbot",
+         admin_id=TOKENS.ADMIN_ID,
+         parent_router=self.event_router,
+         status=f"..help s.ze.ax/d@for@support@{aurflux.utils.UNICODE.circle_sm}@pm@for@invite".replace("@", aurflux.utils.UNICODE.nbsp)
+      )
 
-    async def startup(self, token: str):
-        await self.flux.start(token)
+   async def startup(self, token: str):
+      await self.flux.start(token)
 
-    async def shutdown(self):
-        await self.flux.logout()
+   async def shutdown(self):
+      await self.flux.logout()
 
 
 pinbot = Pinbot()
-
-
-
-
 
 pinbot.flux.register_cog(PinHandler)
 pinbot.flux.register_cog(Interface)
